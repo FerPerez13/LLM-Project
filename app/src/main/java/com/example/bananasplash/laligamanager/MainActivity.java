8262daa80c7142fbf25953fb9b5b95993e1041b0
Fernando Pérez <fer13vlc@gmail.com>
2018-04-09 02:54:49 +0200

    Añadida autentificacion Email/contraseña Firebase

--- app/src/main/java/com/example/bananasplash/laligamanager/MainActivity.java
+++ app/src/main/java/com/example/bananasplash/laligamanager/MainActivity.java
@@ -2,33 +2,62 @@ package com.example.bananasplash.laligamanager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    Log.i("SESION", "Sesion Iniciada con email: "+user.getEmail());
+                    Intent intent = new Intent(getApplicationContext(), LeagueListActivity.class);
+                    startActivity(intent);
+                } else {
+                    Log.i("SESION", "Sesion Cerrada");
+                }
+            }
+        };
 
         //LOG-IN
         Button buttonLogin = (Button)findViewById(R.id.buttonLogin);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println("Comprobamos y vamos a Selector de ligas");
-                //TODO: Lanzar la activity donde leemos las ligas en las que está registrado
-                Intent intent = new Intent(MainActivity.this, LeagueListActivity.class);
-                startActivity(intent);
+                EditText email = findViewById(R.id.email);
+                EditText password = findViewById(R.id.password);
+
+                String emailT = email.getText().toString();
+                String passwordT = password.getText().toString();
+
+                register(emailT, passwordT);
             }
         });
 
@@ -37,10 +66,13 @@ public class MainActivity extends AppCompatActivity {
         buttonSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println("Lanzamos layout flotante de registro");
-                //TODO: Falta el layout de registro en una ventana flotante (signIn Activity)
-                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
-                startActivity(intent);
+                EditText email = findViewById(R.id.email);
+                EditText password = findViewById(R.id.password);
+
+                String emailT = email.getText().toString();
+                String passwordT = password.getText().toString();
+
+                signIn(emailT, passwordT);
             }
         });
 
@@ -66,5 +98,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void register(String email, String password){
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Log.i("SESSION", "Usuario creado correctamente");
+                } else {
+                    Log.i("SESSION", task.getException().getMessage()+"");
+                }
+
+            }
+        });
+    }
+    private void signIn(String email, String password){
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(mAuthListener!=null)
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
     }
 }
---
+++ app/src/main/java/com/example/bananasplash/laligamanager/agregates/References.java
@@ -0,0 +1,5 @@
+package com.example.bananasplash.laligamanager.agregates;
+
+public class References {
+    final public static String LEAGUES_REFERENCE = "leagues";
+}
